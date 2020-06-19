dcd6a6b961b4ba5425d3afa9fd12951ba230c1f3
anubhvshrma18 <57109153+anubhvshrma18@users.noreply.github.com>
2020-06-20 05:02:44 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,139 @@
+
+// Change the navbar button color on click
+const navClass = document.querySelector(".nav")
+const navLink = navClass.querySelectorAll("a");
+function colortoggler(){
+  navLink[0].style.color = "aliceblue";
+  navLink[0].style.borderBottom = "none";
+
+    if (this.style.color != "rgb(114, 250, 234)"){
+        navLink.forEach(
+            function (item){
+                if (item.style.color == "rgb(114, 250, 234)"){
+                    item.style.color = "aliceblue";
+                    item.style.borderBottom = "none";
+                }
+
+            }
+        );
+        this.style.color = "rgb(114, 250, 234)";
+        this.style.borderBottom = "1px solid rgb(114, 250, 234)"
+
+    }
+    
+}
+navLink.forEach((item) => item.addEventListener("click", colortoggler))
+
+
+var i = 0;
+var txt = `ANUBHAV SHARMA.`; /* The text */
+var speed = 300; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("name").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+    
+  }
+}
+typeWriter();
+
+
+let html =  document.querySelector(".html");
+let css =  document.querySelector(".css");
+let js =  document.querySelector(".js");
+let bootstrap =  document.querySelector(".bootstrap");
+let skills = document.querySelector(".progress");
+// console.table(skills);
+
+let finalh = () => {
+  html.style.width = "95%";
+  html.style.transition = "all 2s linear";
+  // setTimeout(()=>html.innerHTML = "<p>80%</p>" ,2000);
+  }
+let finalc = () => {
+  css.style.width = "95%";
+  css.style.transition = "all 2s linear";
+  // setTimeout(()=>css.innerHTML = "<p>70%</p>" ,2000);
+  }
+let finalj = () => {
+  js.style.width = "85%";
+  js.style.transition = "all 2s linear";
+  // setTimeout(()=>js.innerHTML = "<p>60%</p>" ,2000);
+  }
+let finald = () => {
+  bootstrap.style.width = "85%";
+  bootstrap.style.transition = "all 2s linear";
+  // setTimeout(()=>bootstrap.innerHTML = "<p>50%</p>" ,2000);
+  }
+
+function progressbar(){
+  html.style.width = "1%";
+  html.style.transition = "none";
+  html.innerHTML = "";
+  setTimeout(finalh, 5);
+
+  css.style.width = "1%";
+  css.style.transition = "none";
+  css.innerHTML = "";
+  setTimeout(finalc, 5)
+
+  js.style.width = "1%";
+  js.style.transition = "none";
+  js.innerHTML = "";
+  setTimeout(finalj, 5)
+
+  bootstrap.style.width = "1%";
+  bootstrap.style.transition = "none";
+  bootstrap.innerHTML = ""
+  setTimeout(finald, 5)
+    
+};
+
+function testingh(){
+  html.style.width = "1%";
+  html.style.transition = "none";
+  html.innerHTML = "";
+  html.removeEventListener("mouseover",testingh);
+
+  setTimeout(finalh, 5);
+  setTimeout(()=>html.addEventListener("mouseover",testingh),2050)
+  
+
+}
+function testingc(){
+  css.removeEventListener("mouseover",testingc);
+  css.style.width = "1%";
+  css.style.transition = "none";
+  css.innerHTML = "";
+  setTimeout(finalc, 5);
+  setTimeout(()=>css.addEventListener("mouseover",testingc),2050)
+  
+
+}
+function testingj(){
+  js.removeEventListener("mouseover",testingj);
+  js.style.width = "1%";
+  js.style.transition = "none";
+  js.innerHTML = "";
+  setTimeout(finalj, 5);
+  setTimeout(()=>js.addEventListener("mouseover",testingj),2050)
+  
+
+}
+function testingd(){
+  bootstrap.removeEventListener("mouseover",testingd);
+  bootstrap.style.width = "1%";
+  bootstrap.style.transition = "none";
+  bootstrap.innerHTML = "";
+  setTimeout(finald, 5);
+  setTimeout(()=>bootstrap.addEventListener("mouseover",testingd),2050)
+  
+
+}
+skills.addEventListener("mousedown", progressbar);
+html.addEventListener("mouseover", testingh);
+css.addEventListener("mouseover", testingc);
+js.addEventListener("mouseover", testingj);
+bootstrap.addEventListener("mouseover", testingd);
